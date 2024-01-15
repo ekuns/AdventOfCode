fb7e503cea59504fdb4e16bcc09c9621905dc597
Edward Kuns <eddie.kuns@gmail.com>
2024-01-15 00:41:40 -0600

    Finish day 10

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,4 +1,5 @@
 from os import path
+from copy import deepcopy
 
 with open('inputs/input10.txt', 'r') as f:
     full_input_lines = f.readlines()
@@ -19,6 +20,7 @@ class Day10:
         self.initial_beast_pos = self.beast_pos
         self.row_count = len(self.board)
         self.col_count = len(self.board[0])
+        self.expanded_board = []
 
     @staticmethod
     def read_board(lines):
@@ -28,6 +30,10 @@ class Day10:
 
     def dump_board(self):
         for row in self.board:
+            print(''.join(row))
+
+    def dump_expanded_board(self):
+        for row in self.expanded_board:
             print(''.join(row))
 
     def get_first_moves(self):
@@ -84,6 +90,71 @@ class Day10:
                 print(f'Simultaneous arrival after {next_moves[0][3]} moves')
                 break
 
+    def make_expanded_board(self):
+        new_row = ['.', ' '] * self.col_count + ['.']
+        for row in self.board:
+            self.expanded_board.append(deepcopy(new_row))
+            self.expanded_board.append([y for x in row for y in [' ', x]] + ['.'])
+
+        # Fill in missing items of the expanded board
+        for row_num, row in enumerate(self.expanded_board):
+            for col_num, col in enumerate(row):
+                if col == ' ':
+                    above = self.expanded_board[row_num - 1][col_num] if row_num > 0 else '.'
+                    below = self.expanded_board[row_num + 1][col_num] if row_num < 2 * self.row_count - 1 else '.'
+                    left = self.expanded_board[row_num][col_num - 1] if col_num > 0 else '.'
+                    right = self.expanded_board[row_num][col_num + 1] if col_num < 2 * self.col_count - 1 else '.'
+                    # Only one connector will fit in this box
+                    above = above == '7' or above == 'F' or above == '|' or above == 'S'
+                    below = below == 'J' or below == 'L' or below == '|' or below == 'S'
+                    right = right == '7' or right == 'J' or right == '-' or right == 'S'
+                    left = left == 'F' or left == 'L' or left == '-' or left == 'S'
+                    if above and below:
+                        self.expanded_board[row_num][col_num] = '|'
+                    elif above and right:
+                        self.expanded_board[row_num][col_num] = 'L'
+                    elif above and left:
+                        self.expanded_board[row_num][col_num] = 'J'
+                    elif below and right:
+                        self.expanded_board[row_num][col_num] = 'F'
+                    elif below and left:
+                        self.expanded_board[row_num][col_num] = '7'
+                    elif right and left:
+                        self.expanded_board[row_num][col_num] = '-'
+                    else:
+                        self.expanded_board[row_num][col_num] = '.'
+
+    def collapse_expanded_board(self):
+        del self.expanded_board[0::2]
+        for row in self.expanded_board:
+            del row[0::2]
+
+    def flood_fill(self, start_row, start_col):
+        queue = [(start_row, start_col)]
+        while queue:
+            row, col = queue.pop()
+            if self.expanded_board[row][col] != '.':
+                continue
+            self.expanded_board[row][col] = 'O'
+            if row > 0:
+                queue.append((row - 1, col))
+            if row < len(self.expanded_board) - 1:
+                queue.append((row + 1, col))
+            if col > 0:
+                queue.append((row, col - 1))
+            if col < len(self.expanded_board[0]) - 1:
+                queue.append((row, col + 1))
+
+    def annotate_inside_tiles(self):
+        count = 0
+        for row in range(len(self.expanded_board)):
+            for col in range(len(self.expanded_board[0])):
+                inside = self.expanded_board[row][col] == '.'
+                self.expanded_board[row][col] = 'I' if inside else '.'
+                if inside:
+                    count += 1
+        return count
+
     def part2(self):
         # Identify the loop so we can clean out everything not part of the loop
         next_moves = self.get_first_moves()
@@ -101,7 +172,16 @@ class Day10:
             for col in range(self.col_count):
                 if (row, col) not in clean_move_history:
                     self.board[row][col] = '.'
-        self.dump_board()
+        # self.dump_board()
+        self.make_expanded_board()
+        # self.dump_expanded_board()
+        self.flood_fill(0, 0)
+        # self.dump_expanded_board()
+        self.collapse_expanded_board()
+        # self.dump_expanded_board()
+        inside_tile_count = self.annotate_inside_tiles()
+        self.dump_expanded_board()
+        print(f'Inside tiles count: {inside_tile_count}')
 
 
 if path.isfile('test_inputs/test10_A1.txt'):
@@ -118,8 +198,15 @@ if path.isfile('test_inputs/test10_A2.txt'):
         print('\nTEST INPUT PART 1B')
         Day10(test_input_lines).part1()
 
-if path.isfile('test_inputs/test10_A1.txt'):
-    with open('test_inputs/test10_A1.txt', 'r') as f:
+if path.isfile('test_inputs/test10_B1.txt'):
+    with open('test_inputs/test10_B1.txt', 'r') as f:
+        test_input_lines = f.readlines()
+        test_input_lines = [l.strip() for l in test_input_lines]
+        print('\nTEST INPUT PART 2')
+        Day10(test_input_lines).part2()
+
+if path.isfile('test_inputs/test10_B2.txt'):
+    with open('test_inputs/test10_B2.txt', 'r') as f:
         test_input_lines = f.readlines()
         test_input_lines = [l.strip() for l in test_input_lines]
         print('\nTEST INPUT PART 2')
